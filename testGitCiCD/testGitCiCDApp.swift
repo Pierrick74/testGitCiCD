1ee458a5e7c74cd7dd26d8369ebd17daaa74ce26
pierrick <pierrickviret@gmail.com>
2024-10-25 21:28:22 +0200

    Initial Commit

---
+++ testGitCiCD/testGitCiCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitCiCDApp.swift
+//  testGitCiCD
+//
+//  Created by pierrick viret on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct testGitCiCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
